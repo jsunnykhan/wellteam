5e009cc9d43aa8d83ef123ce3d20fb69855f6cae
jsunnykhan <a.ekush045@gmail.com>
2022-02-10 02:57:08 +0600

    fix maybe

--- components/challenge/Challenge.tsx
+++ components/challenge/Challenge.tsx
@@ -5,19 +5,19 @@ import ChallengeLeftSection from "./ChallengeLeft";
 function Challenge() {
   return (
     <div id="challenge" className="pl-[10%] relative">
-      <div className="w-[50%] left-0 top-0">
+      <div className="w-[45%] left-0 top-0">
         <ChallengeLeftSection />
       </div>
 
-      <div className="absolute right-0 top-20">
+      <div className="absolute right-0 top-20 w-[40%]">
         <Image
           src="/4.jpg"
           alt="travel world"
           width={500}
-          height={420}
-          layout="fixed"
+          height={350}
+          layout="responsive"
         />
-        <div className="absolute w-full h-full top-0 bg-black opacity-30"></div>
+        <div className="absolute w-full h-full top-0 right-0 bottom-0 left-0 bg-black opacity-30"></div>
       </div>
     </div>
   );
